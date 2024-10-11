1472d32791001d36a7c25d7d2d2f5f37f99fe83f
mychidarko <mickdd22@gmail.com>
2024-10-11 14:48:02 +0000

    feat: add install command

--- src/Commands/InstallCommand.php
+++ src/Commands/InstallCommand.php
@@ -9,59 +9,75 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 class InstallCommand extends Command
 {
-	/**
-	 * Configure the command options.
-	 *
-	 * @return void
-	 */
-	protected function configure()
-	{
-		$this
-			->setName('install')
-			->setDescription('Generate base alchemy files')
-			->addOption('force', 'f', InputOption::VALUE_NONE, 'Replace alchemy files if they already exist');
-	}
+    /**
+     * Configure the command options.
+     *
+     * @return void
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('install')
+            ->setDescription('Generate base alchemy files')
+            ->addOption('force', 'f', InputOption::VALUE_NONE, 'Replace alchemy files if they already exist');
+    }
 
-	/**
-	 * Execute the command.
-	 *
-	 * @param InputInterface $input
-	 * @param OutputInterface $output
-	 * @return int
-	 */
-	protected function execute(InputInterface $input, OutputInterface $output): int
-	{
-		$appAlchemyFile = (getcwd() . '/alchemy.yml');
-		if (!file_exists($appAlchemyFile) || (file_exists($appAlchemyFile) && !$input->getOption('force'))) {
-			copy(
-				dirname(__DIR__) . '/setup/pest/alchemy.yml',
-				getcwd() . '/alchemy.yml',
-			);
-		}
+    /**
+     * Execute the command.
+     *
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return int
+     */
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $appAlchemyFile = getcwd() . '/alchemy.yml';
 
-		$this->updateComposerJson();
+        if (!file_exists($appAlchemyFile) || (file_exists($appAlchemyFile) && !$input->getOption('force'))) {
+            copy(
+                dirname(__DIR__) . '/setup/alchemy.yml',
+                getcwd() . '/alchemy.yml',
+            );
+        }
 
-		$output->writeln('<info>Alchemy installed successfully.</info>');
+        $this->updateComposerJson();
+        $this->updateGitIgnore();
 
-		return 0;
-	}
+        $output->writeln('<info>Alchemy installed successfully.</info>');
 
-	protected function updateComposerJson()
-	{
-		$appComposerJson = json_decode(file_get_contents(getcwd() . '/composer.json'), true);
+        return 0;
+    }
 
-		$composerConfig = $appComposerJson['config'] ?? [];
-		$composerConfigPlugins = $composerConfig['allow-plugins'] ?? [];
+    protected function updateComposerJson()
+    {
+        $appComposerJson = json_decode(file_get_contents(getcwd() . '/composer.json'), true);
 
-		$appComposerJson['scripts']['alchemy'] = './vendor/bin/alchemy setup';
-		$appComposerJson['scripts']['test'] = './vendor/bin/alchemy test';
-		$appComposerJson['scripts']['lint'] = './vendor/bin/alchemy lint';
-		$appComposerJson['config'] = array_merge($composerConfig, [
-			'allow-plugins' => array_merge($composerConfigPlugins, [
-				'pestphp/pest-plugin' => true,
-			]),
-		]);
+        $composerConfig = $appComposerJson['config'] ?? [];
+        $composerConfigPlugins = $composerConfig['allow-plugins'] ?? [];
 
-		file_put_contents(getcwd() . '/composer.json', json_encode($appComposerJson, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES));
-	}
+        $appComposerJson['scripts']['alchemy'] = './vendor/bin/alchemy setup';
+        $appComposerJson['scripts']['test'] = './vendor/bin/alchemy test';
+        $appComposerJson['scripts']['lint'] = './vendor/bin/alchemy lint';
+        $appComposerJson['config'] = array_merge($composerConfig, [
+            'allow-plugins' => array_merge($composerConfigPlugins, [
+                'pestphp/pest-plugin' => true,
+            ]),
+        ]);
+
+        file_put_contents(getcwd() . '/composer.json', json_encode($appComposerJson, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES));
+    }
+
+    protected function updateGitIgnore()
+    {
+        $appGitIgnoreFile = getcwd() . '/.gitignore';
+        $gitIgnoreContent = file_exists($appGitIgnoreFile) ? file_get_contents($appGitIgnoreFile) : '';
+
+        if (strpos($gitIgnoreContent, '.alchemy') === false) {
+            file_put_contents($appGitIgnoreFile, "\n# Alchemy\n.alchemy\n", FILE_APPEND);
+        }
+
+        if (strpos($gitIgnoreContent, '.phpunit.result.cache') === false) {
+            file_put_contents($appGitIgnoreFile, ".phpunit.result.cache\n", FILE_APPEND);
+        }
+    }
 }
